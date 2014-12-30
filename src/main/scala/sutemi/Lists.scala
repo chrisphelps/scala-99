f3f7af5bd305f2f82b575cbd5dca626a4a15ee7d
Chris Phelps <cphelps@tendrilinc.com>
2014-12-30 16:58:52 -0700

    Second problem: last but one element of a list

--- src/main/scala/sutemi/Lists.scala
+++ src/main/scala/sutemi/Lists.scala
@@ -13,4 +13,12 @@ object Lists {
     case x::Nil => Some(x)
     case _::xs => last(xs)
   }
+
+  @tailrec
+  def penultimate[T](list: List[T]): Option[T] = list match {
+    case Nil => None
+    case x::Nil => None
+    case x::_::Nil => Some(x)
+    case _::xs => penultimate(xs)
+  }
 }
--- src/test/scala/sutemi/ListSuite.scala
+++ src/test/scala/sutemi/ListSuite.scala
@@ -12,4 +12,9 @@ class ListSuite extends FunSuite {
     assert(Lists.last(List()) === None)
     assert(Lists.last(List(1,2,3,4,5)) === Some(5))
   }
+
+  test("problem 2: last but one element of a list") {
+    assert(Lists.penultimate(List(1)) === None)
+    assert(Lists.penultimate(List(1, 1, 2, 3, 5, 8)) === Some(5))
+  }
 }
