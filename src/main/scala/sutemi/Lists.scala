0d9f330b6841253ba6eca0e0c10cf7299b7eae01
Chris Phelps <cphelps@tendrilinc.com>
2014-12-30 16:55:34 -0700

    First problem: last element of a list

--- src/main/scala/sutemi/Lists.scala
+++ src/main/scala/sutemi/Lists.scala
@@ -1,8 +1,16 @@
 package sutemi
 
+import scala.annotation.tailrec
+
 
 object Lists {
 
   def hello = "Hello"
 
+  @tailrec
+  def last[T](list: List[T]): Option[T] = list match {
+    case Nil => None
+    case x::Nil => Some(x)
+    case _::xs => last(xs)
+  }
 }
--- src/test/scala/sutemi/ListSuite.scala
+++ src/test/scala/sutemi/ListSuite.scala
@@ -8,4 +8,8 @@ class ListSuite extends FunSuite {
     assert(Lists.hello === "Hello")
   }
 
+  test("problem 1: last element of a list") {
+    assert(Lists.last(List()) === None)
+    assert(Lists.last(List(1,2,3,4,5)) === Some(5))
+  }
 }
